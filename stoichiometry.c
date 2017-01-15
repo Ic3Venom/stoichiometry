0db94fbdb67e3b86a34cf6930179e37ace22aaf6
Ic3Venom <Ic3Venom@gmail.com>
2017-01-15 14:39:30 -0800

    Etiquette change, name change, and program update

--- stoichiometry.c
+++ stoichiometry.c
@@ -1,10 +1,18 @@
+/*
+** File: stoichiometry.c
+** Calculates some basic numbers from an equation and amounts of chemicals
+** Created by Julian Meyn, 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define INPUT_LEN   51
 #define CPD_LEN     5
 
-//Struct contains information about an element or a molecule
+/*
+**Struct contains information about an element or a molecule
+*/
 struct ELEMENT{
     int     amount;
     int     number;
@@ -13,20 +21,28 @@ struct ELEMENT{
     float   mass;
 };
 
-//COMPOUND is the combination of 2 or more different types of elements
-//A compound can contain multiple compounds, stored in linked list CPD_NODE
+/*
+** COMPOUND is the combination of 2 or more different types of elements
+** A compound can contain multiple compounds, stored in linked list CPD_NODE
+*/
 struct COMPOUND{
     int     amount;
     float   massTotal;
     struct  ELEMENT elements[CPD_LEN];
 };
 
+/*
+** Linked list of compounds
+*/
 struct CPD_NODE{
-    struct COMPOUND;
+    int ID;
+    struct COMPOUND compound;
     struct CPD_NODE *next;
-} *root, *cpd1;
+} *root, *conductor;
 
-//Altered code, originally from 'ultimatetypingtest.cpp'
+/*
+** Altered code, originally from 'ultimatetypingtest main.c'
+*/
 int stringToInt(char character)
 {
     switch (character)
@@ -40,13 +56,32 @@ int stringToInt(char character)
     case '7': return 7;
     case '8': return 8;
     case '9': return 9;
+    case '0': return 0;
     case '+': return 10;
     case '-': return 11;
     case ' ': return 12;
-    default: return 0;
+    default: return 13;
     }
 }
 
+/*
+** Altered exponent function, originally from 'baseconverter main.c'
+*/
+int exponentiate(short base, short exponent)
+{
+    int power = 1;
+
+    while(exponent)
+    {
+        power *= base;
+        exponent--;
+    }
+    return power;
+}
+
+/*
+** Finds atomic mass of 'chemical' and multiplies it by 'amount'
+*/
 float elementFind(char* chemical, int amount)
 {
     struct ELEMENT element;
@@ -77,12 +112,17 @@ float elementFind(char* chemical, int amount)
     return element.mass * amount;
 }
 
+/*
+** UNFINISHED
+*/
 void compoundRead(char chemical[CPD_LEN])
 {
 
 }
 
-//Seems justified to separate this large amount of text into a separate function
+/*
+** Large block of text from help menu
+*/
 void help()
 {
     printf("\n-----------------------------------<HELP>----------------------------------\n");
@@ -95,11 +135,15 @@ void help()
     printf("\n---------------------------------------------------------------------------\n\n");
 }
 
+/*
+**Takes input from reader and interprets it, preparing for product creation
+*/
 void inputRead()
 {
-    int  j, i;
+    int  i, j, currentChar;
     char input[INPUT_LEN];
 
+    //Program UI
     while(1)
     {
         printf("Input your BALANCED chemical equation (type 'help' for help):\n$ ");
@@ -114,21 +158,68 @@ void inputRead()
         break;
     }
 
-    /*
-    Linked list is just a concept, not implemented correctly, so this is incomplete
-
-    root = (struct node *) malloc( sizeof(struct node) );
-        root -> next = 0;
-
-    */
+    root        = (struct CPD_NODE *) malloc( sizeof(struct CPD_NODE) );
+    root->next  = 0;
+    conductor   = root;
 
     for(i = 0, j = 0; i < INPUT_LEN; i++)
     {
+        currentChar = stringToInt( input[i] );
 
+        if(currentChar >= 0 &&
+           currentChar <= 9)
+        {
+            while(1)
+            {
+                currentChar = stringToInt( input[j] );
+                conductor->compound.amount = 0;
+
+                //This is tested twice for the first number
+                if (currentChar >= 0 &&
+                    currentChar <= 9)
+                {
+                    j++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            while(j)
+            {
+                j--;
+
+                currentChar = stringToInt( input[j] );
+
+                conductor->compound.amount += (currentChar * exponentiate(10, j) );
+                //printf("j: %d, %d, %d\n", j, (currentChar * exponentiate(10, j) ), currentChar );
+            }
+        }
+        else
+        {
+            switch(currentChar):
+            {
+            case 10:
+
+            case 11:
+
+            case 12:
+
+            case 13:
+
+            default:
+                printf("\nSomething went wrong! Report this with proof at the GitHub issues page");
+                exit(-1);
+            }
+        }
     }
-
+    printf("\nConductor->compound.amount: %d", conductor->compound.amount);
 }
 
+/*
+** Program start
+*/
 int main()
 {
     inputRead();
