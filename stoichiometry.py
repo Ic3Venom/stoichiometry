0d17623ae251cabc6c4a07be9f4c1f62a0aa0664
Ic3Venom <Ic3Venom@gmail.com>
2017-01-18 18:13:55 -0800

    Basic, not functional starting program

---
+++ stoichiometry.py
@@ -0,0 +1,23 @@
+class Element:
+
+    def __init__(self, name):
+        self.name = name
+        print name
+    
+    def elementFind():
+        f = open('periodictable.txt', 'r')
+
+class compound():
+    exit
+def compoundRead():
+    exit
+def inputRead():
+    exit
+    
+if __name__ == '__main__':
+
+    element = Element("H")
+
+    element.elementFind
+
+    exit
