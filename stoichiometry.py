bfd819ecc5169843264ee61d43bcaf1ba09097c3
Ic3Venom <Ic3Venom@gmail.com>
2017-01-19 19:53:41 -0800

    Fragments of classes, some debugging

--- stoichiometry.py
+++ stoichiometry.py
@@ -1,23 +1,65 @@
-class Element:
-
-    def __init__(self, name):
-        self.name = name
-        print name
+class Info:
+    '''Holds information of Elements and Compounds'''
     
-    def elementFind():
+    def __init__(self):
+        self.data = {}
+        self.data = self.data.fromkeys(
+            ['number','name','symbol','mass','amount'])
+    
+    def change(self, key, value):
+        '''Changes one of the data values'''
+        for i in self.data:
+            if i == key:
+                self.data[key] = value
+                return self.data[key]
+    
+    def number(self):
+        return self.data['number']
+    def name(self):
+        return self.data['name']
+    def symbol(self):
+        return self.data['symbol']
+    def mass(self):
+        return self.data['mass']
+    def amount(self):
+        return self.data['amount']
+    
+class Element:
+    '''A class to hold element information'''
+
+    def __init__(self, symbol):
+        self.stat  = Info()
+
+    def elementFind(self):
+        '''Finds element in periodictable.txt'''
         f = open('periodictable.txt', 'r')
 
-class compound():
-    exit
-def compoundRead():
-    exit
-def inputRead():
-    exit
+        for line in f:
+            for word in line.split():
+                print word,
+
+        f.close()
+
+class Compound():
+    '''A class to hold compounds, which hold class(Element)'''
+    
+    def __init__(self, symbol):
+        self.stat  = Info()
+    
+def compoundAnalyze():
+    pass
+
+def inputAnalyze():
+    pass
     
 if __name__ == '__main__':
 
-    element = Element("H")
+    print "Input your BALANCED chemical equation (type 'help' for help):"
+    userInput = raw_input(">>> ")
 
-    element.elementFind
+    element = Element('H')
+
+    element.stat.change('name', 'hydrogen')
+    print element.stat.name()
 
     exit
