b9f395ef95d65a7b51ff7ebc49e85ca898354adc
Ic3Venom <Ic3Venom@gmail.com>
2017-01-29 22:38:03 -0800

    Lot of concepts spreaded around
    Nothing put in works (I believe), trying to figure out how to analyze any compound (including nested compounds).

--- stoichiometry.py
+++ stoichiometry.py
@@ -32,6 +32,11 @@ class Info:
 class Element:
     '''A class to hold element information'''
 
+    def amount(self):
+        for i in range( len(self.stat.symbol) ):
+            pass
+            
+    
     def find(self, value):
         '''Finds element in periodictable.txt, returns element mass'''
         f = open('periodictable.txt', 'r')
@@ -45,35 +50,102 @@ class Element:
                     f.close()
                     return line.split()[maxSearch]
         else:
-            print 'Could not find element %s! Exiting program' % self.stat.symbol()
+            print 'ERROR: unknown element %s. Exiting program' % self.stat.symbol()
             f.close()
             exit(1)
 
-    def __init__(self, symbol):
+    def __init__(self, symbol, totalAmount):
         self.stat = Info()
         self.stat.symbol = symbol
+        self.stat.amount = amount * self.amount()
 
 class Compound():
     '''A class to hold compounds, which hold class(Element)'''
-    def amount(self):
-        for i in range(len(self.stat.symbol)):
+
+    def analyze(self):
+        '''Determines interior elements and compounds and puts them in array(inside)'''
+        j = 0
+        
+        for i in range( len( str(self.stat.amount) ), len(self.stat.symbol) ):
+            print 'i: %d, %s' % (i, self.stat.symbol[i])
+
+            #if: i is a bracket, then then find subscript of bracket or find more brackets
+            '''if self.stat.symbol[i] == '(':
+                bracketCount  = 0
+                bracketAmount = 0
+                
+                while True:
+                    if self.stat.symbol[i + j] == '(':
+                        bracketCount += 1
+                    elif self.stat.symbol[i + j] == ')':
+                        bracketCount -= 1
+                    if bracaketCount == 0:
+                        k = j
+                        while True:
+                            j += 1 
+                            if not self.stat.symbol[i + j].isdecimal():
+                                self.inside.append(
+                                    Element(
+                                        self.stat.symbol[k: k + j],
+                                        self.stat.amount * self.stat.symbol[i + j: k + j)
+
+
+                while True:                    
+                    self.stat.symbol[j] != ')'
+                    j += 1
+
+                    if j > len( self.stat.symbol ):
+                        print 'ERROR: unable to end parenthesis \')\'. Exiting program'
+                        exit(1)'''
+
+            
+            if self.stat.symbol[i].isupper() and not j == 0:
+                self.inside.append(
+                    Element(
+                        self.stat.symbol[len(str.stat.symbol) - i: j + 1],
+                        self.stat.amount) )
+
+            #if: i is decimal, continue until Capital Letter or end of Compound
+            if self.stat.symbol[i].isdecimal():
+                while True:
+
+                    if j > len( self.stat.symbol ):
+                        pass
+
+            '''self.elements.append(
+                Element(
+                    self.stat.symbol[j: i],
+                    self.stat.amount)'''
+
+    def coef(self):
+        for i in range( len(self.stat.symbol) ):
             currentChar = self.stat.symbol[i]
 
             if not currentChar.isdigit():
                 break
 
+        #if Compound has no coefficent, amount defaults to 1
         if not self.stat.symbol[0].isdigit():
             return 1
         else:
             return int( self.stat.symbol[0:i] )
-    def analyze(self):
-        pass    
-    
-    def __init__(self, symbol):
-        self.stat  = Info()
-        self.stat.symbol = symbol
-        self.stat.amount = self.amount()
+        
+    def elementLen(self):
+        j = 0
+        for i in self.stat.symbol:
+            if i.isupper():
+                j += 1
 
+        return j
+
+    def __init__(self, symbol):
+        self.stat = Info()
+        self.stat.symbol = symbol
+        self.stat.amount = self.coef()
+
+        self.inside = []
+        self.analyze()
+ 
 if __name__ == '__main__':
     compoundList = [ ]
 
